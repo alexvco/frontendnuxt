bf1a74a1c2c9efd693ea0ecc5e98f7052c4d1c87
Alex Varjabedian <alex@trueandco.com>
2020-09-14 01:36:57 -0700

    re-create home (index) page and add nav (AppHeader) to be on every page (inside layout)

---
+++ components/AppHeader.vue
@@ -0,0 +1,36 @@
+<template>
+  <header>
+    <h1>Frontendnuxt</h1>
+    <ul>
+      <li>
+        <nuxt-link to="/">Home</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/">Home</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/">Home</nuxt-link>
+      </li>
+    </ul>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader'
+}
+</script>
+
+<style>
+
+li {
+  list-style: none;
+  display: inline-block;
+}
+.header ul {
+  display: flex;
+}
+.header a {
+  display: inline-block;
+}
+</style>
