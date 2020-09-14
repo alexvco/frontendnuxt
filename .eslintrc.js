a4a9e8e098ad5aae67e89b3508704acf3bcd9cba
Alex Varjabedian <alex@trueandco.com>
2020-09-14 00:59:28 -0700

    add a rule in eslint to not require space before function parenthesis

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,11 @@ module.exports = {
   plugins: [
   ],
   // add your custom rules here
-  rules: {}
+  rules: {
+    'space-before-function-paren': ['error', {
+      anonymous: 'ignore',
+      named: 'ignore',
+      asyncArrow: 'ignore'
+    }]
+  }
 }
