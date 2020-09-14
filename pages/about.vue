8032f6c5a23a43ba3799e53616db8dcad922a367
Alex Varjabedian <alex@trueandco.com>
2020-09-14 00:58:59 -0700

    add a static about_us page (automatically routes to /about) with title and meta description

---
+++ pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>About Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'About Us',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'This is the about us page'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
