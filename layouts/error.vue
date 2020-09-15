40c215925024846ec1b9a233ad488abfc992a3c7
Alex Varjabedian <alex@trueandco.com>
2020-09-15 01:58:42 -0700

    error page with message

---
+++ layouts/error.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    <h1>An error occurred: {{ error.message }}</h1>
+    <nuxt-link to="/">Home page</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error'],
+  layout: 'default'
+}
+</script>
