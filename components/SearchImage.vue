121c6b5c292f9434ced4da35a0623875e56c0804
Alex Varjabedian <alex@trueandco.com>
2020-09-15 02:37:37 -0700

    Search functionality

---
+++ components/SearchImage.vue
@@ -0,0 +1,27 @@
+<template>
+  <form @submit.prevent="submitSearch">
+    <input type="text" v-model="imagetext" name="" placeholder="Search Images...">
+    <input type="submit" name="" value="Search">
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'SearchImage',
+  data() {
+    return {
+      imagetext: ''
+    }
+  },
+  methods: {
+    submitSearch() {
+      this.$emit('search-text', this.imagetext)
+      this.imagetext = ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- pages/images/index.vue
+++ pages/images/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>Images</h1>
+    <v-search-image v-on:search-text="searchForImage" />
     <v-pic v-for="pic in pics" :key="pic.id" :id="pic.id" :picfilename="pic.attributes.filename" />
   </div>
 </template>
@@ -8,10 +9,12 @@
 <script>
 import axios from 'axios'
 import Pic from '~/components/Pic'
+import SearchImage from '~/components/SearchImage'
 
 export default {
   components: {
-    vPic: Pic
+    vPic: Pic,
+    vSearchImage: SearchImage
   },
   data() {
     return {
@@ -30,6 +33,22 @@ export default {
       this.pics = res.data.data
     } catch (err) {
       console.log(err)
+    }
+  },
+  methods: {
+    async searchForImage(text) {
+      const config = {
+        headers: {
+          Accept: 'application/json'
+        }
+      }
+
+      try {
+        const res = await axios.get(`http://localhost:3000/api/v1/images?term=${text}`, config)
+        this.pics = res.data.data
+      } catch (err) {
+        console.log(err)
+      }
     }
   },
   head() {
